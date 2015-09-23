12535710394350529096045af2df486f9390edea
Keith Yong <terda12@gmail.com>
2015-09-23 12:07:11 -0400

    First initial node commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-var express = require("express");
+var express = require('express');
+var config = require('./config')
 var app = express();
 
 var router = express.Router();
 
+app.use('/timesheet', require('./routes/timesheet'));
 router.get('/', (req, res, next) => {
 
 });
+
 router.post('/punch/in', (req, res, next) => {
     var body = req.body;
     console.log(body);
     // Add to DB.
-};
+});
+
+app.listen(config.port);
